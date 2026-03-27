b249daf1733bfc32b32862fbf583b66926ef89ba
Noor Nabeeha K <measascholar@gmail.com>
2026-03-27 18:52:46 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import json
-from dotenv import load_dotenv
-load_dotenv()
-
-from scanners.risk_scoring import getScore
-
-
-result = getScore("scanme.nmap.org")
-print(json.dumps(result, indent=2, default=str))
